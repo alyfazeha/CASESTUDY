babb670713acda2c4843b15df34ab88fd861707a
habyy-sans <muhammadhasbiyashshiddiqi@gmail.com>
2025-04-21 16:31:19 +0700

    matkul

--- dataMatakuliah.java
+++ dataMatakuliah.java
@@ -1,16 +1,21 @@
 public class dataMatakuliah {
-    matakuliah[] dataMatakuliah = new matakuliah[10];
-    int idx = 0;
-
-    public void tambah(matakuliah matkul){
-        if (idx < dataMatakuliah.length){
-            dataMatakuliah[idx] = matkul;
-            idx++;
-        }
-    }
+   // matakuliah[] dataMatakuliah = new matakuliah[];
+    
+    matakuliah[] daftarMK = {
+        new matakuliah("MK001", "Struktur data", 3),
+        new matakuliah("MK002", "Basis Data", 3),
+        new matakuliah("MK003", "Desain Web", 3)
+    };
+    //public void tambah(matakuliah matkul){
+      //  if (idx < dataMatakuliah.length){
+        //    dataMatakuliah[idx] = matkul;
+          //  idx++;
+        //}
+    //}
     public void tampil(){
-        for (int i = 0; i < idx; i++){
-            dataMatakuliah[i].tampil();
+        System.out.println("Daftar Matakuliah: ");
+        for (matakuliah mk : daftarMK){
+        mk.tampil();
         }
     }
 }
--- matakuliahDemo.java
+++ matakuliahDemo.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 public class matakuliahDemo{
-    Scanner sc = new Scanner(System.in);
-    matakuliahDemo data = new matakuliahDemo();
-    while (true) {
-        System.out.println("=== MENU SISTEM AKADEMIK ==");
-        System.out.println("1. Tampilkan Daftar Mahasiswa");
-        System.out.println("2. Tampilkan Daftar Mata kuliah");
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);
+        matakuliahDemo data = new matakuliahDemo();
+        while (true) {
+            System.out.println("=== MENU SISTEM AKADEMIK ==");
+            System.out.println("1. Tampilkan Daftar Mahasiswa");
+            System.out.println("2. Tampilkan Daftar Mata kuliah");
         System.out.println("3. Tampilkan Data Penilaian");
         int pilihan = sc.nextInt();
         sc.nextLine(); 
         switch (pilihan) {
-            
+            case 1:
+            case 2:
+                System.out.println("\nDaftar Mata Kuliah: ");
+                data.tampil();
+                break;
+            }
         }
     }
 }
