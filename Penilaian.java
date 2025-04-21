a4f3ac98a35ac95ede7ca0ec4550a311cb2999f1
alyfazeha <alyfazeha@gmail.com>
2025-04-21 14:50:45 +0700

    bikin file Penilaian.java

---
+++ Penilaian.java
@@ -0,0 +1,39 @@
+public class Penilaian {
+    Mahasiswa mahasiswa;
+    MataKuliah matkul;
+    String tugas;
+    double uts;
+    double uas;
+
+    Penilaian() {
+
+    }
+
+    Penilaian(Mahasiswa mahasiswa, MataKuliah matkul, String tugas, double uts, double uas) {
+        this.mahasiswa = mahasiswa;
+        this.matkul = matkul;
+        this.tugas = tugas;
+        this.uts = uts;
+        this.uas = uas;
+    }
+
+    double nilaiTugas() {
+
+    }
+
+    double nilaiUts() {
+
+    }
+
+    double nilaiUas() {
+
+    }
+
+    double nilaiAkhir() {
+
+    }
+
+    void hitungNilaiAkhir() {
+
+    }
+}
