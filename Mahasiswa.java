47f39061ae2ecb7969256ad01a5b3bd5f8a0d89c
Lovie-Tonimba <jechonialovie@gmail.com>
2025-04-21 16:09:32 +0700

    revisi

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -10,7 +10,7 @@ public class Mahasiswa {
         this.nama = nama;
         this.prodi = prodi;
     }
-    void tampilMahasiswa(Mahasiswa listMhs){
+    void tampilMahasiswa(Mahasiswa [] listMhs){
         System.out.println("\nDaftar Mahasiswa:");
         for (int i = 0; i < 3; i++) {
             System.out.println("NIM: " + listMhs[i].NIM + " | Nama: " + listMhs[i].nama + " | Prodi: " + listMhs[i].prodi);
