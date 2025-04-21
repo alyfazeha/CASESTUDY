434a262b98a22088b083be20b5a1bee4ff6f8b9d
Lovie-Tonimba <jechonialovie@gmail.com>
2025-04-21 16:24:41 +0700

    .

--- Penilaian.java
+++ Penilaian.java
@@ -1,26 +1,26 @@
-public class Penilaian {
-    Mahasiswa mahasiswa;
-    MataKuliah matkul;
-    double tugas;
-    double uts;
-    double uas;
-    double nilaiAkhir;
-    Penilaian(Mahasiswa mahasiswa, MataKuliah matkul, double tugas, double uts, double uas) {
-        this.mahasiswa = mahasiswa;
-        this.matkul = matkul;
-        this.tugas = tugas;
-        this.uts = uts;
-        this.uas = uas;
-        hitungNilaiAkhir();
-    }
+// public class Penilaian {
+//     Mahasiswa mahasiswa;
+//     MataKuliah matkul;
+//     double tugas;
+//     double uts;
+//     double uas;
+//     double nilaiAkhir;
+//     Penilaian(Mahasiswa mahasiswa, MataKuliah matkul, double tugas, double uts, double uas) {
+//         this.mahasiswa = mahasiswa;
+//         this.matkul = matkul;
+//         this.tugas = tugas;
+//         this.uts = uts;
+//         this.uas = uas;
+//         hitungNilaiAkhir();
+//     }
 
-    void hitungNilaiAkhir() {
-        double nilaiAkhir = (0.3 * tugas) + (0.3 * uts) + (0.4 * uas);
-        System.out.println("Nilai Akhir: " + nilaiAkhir);
-    }
+//     void hitungNilaiAkhir() {
+//         double nilaiAkhir = (0.3 * tugas) + (0.3 * uts) + (0.4 * uas);
+//         System.out.println("Nilai Akhir: " + nilaiAkhir);
+//     }
 
-    void tampilkanDataPenilaian() {
-        System.out.println("=== Data Penilaian Mahasiswa ===");
+//     void tampilkanDataPenilaian() {
+//         System.out.println("=== Data Penilaian Mahasiswa ===");
         
-    }
-}
+//     }
+// }
